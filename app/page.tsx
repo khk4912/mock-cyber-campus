d5a0423f32bfb9ad82535debe3035282ddc2b975
khk4912 <kosame@kosame.dev>
2026-04-02 01:47:23 +0900

    🎉

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home () {
+  return (
+    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+      <h1 className='text-4xl font-bold text-gray-900 dark:text-white'>
+        Hello, World!
+      </h1>
+      <p className='mt-4 text-gray-600 dark:text-gray-300' />
+    </div>
+  )
+}
