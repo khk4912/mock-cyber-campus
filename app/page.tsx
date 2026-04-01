f59da97b8a76962420aff45685d91deb9a816f33
khk4912 <kosame@kosame.dev>
2026-04-02 03:12:37 +0900

    🎉feat: Implements NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home () {
   return (
-    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+    <div className='flex flex-col flex-1 items-center justify-center bg-zinc-50 dark:bg-black'>
       <h1 className='text-4xl font-bold text-gray-900 dark:text-white'>
         Hello, World!
       </h1>
---
+++ components/layout/Header.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+
+export function NavBar () {
+  return (
+    <nav
+      className='fixed w-full h-15 flex justify-between
+                bg-gachon-blue text-white
+                px-15 py-8
+                not-md:justify-center'
+    >
+      <div className='self-center not-md:hidden'>
+        <Image
+          src='/logo.svg'
+          alt='가천대학교 로고'
+          width={140} height={40}
+        />
+      </div>
+      <div className='flex gap-4 self-center '>
+        <div className='flex gap-2 items-center'>
+          <Image
+            src='/images/f2.png'
+            alt='프로필 이미지'
+            width={30} height={30}
+            className='rounded-full object-cover'
+          />
+          <span className='text-xs'>학생 모드 </span>
+        </div>
+        <button
+          className='bg-gray-500 text-white p-3 y-fit
+                      text-xs rounded-2xl
+                      cursor-pointer
+                      hover:bg-gray-400 transition-colors'
+        >
+          전환하기
+        </button>
+      </div>
+    </nav>
+  )
+}
