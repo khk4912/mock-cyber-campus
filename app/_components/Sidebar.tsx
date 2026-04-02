16ecc73d91ce36af34b61ae8930c3625fabfc219
khk4912 <kosame@kosame.dev>
2026-04-02 17:14:48 +0900

    💄 chore: Add y-divider

--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -7,12 +7,14 @@ type ItemProps = {
 function Item (props: ItemProps) {
   const { title, iconSrc } = props
   return (
-    <li className='flex gap-4 h-10 items-center'>
+    <li className='side-item flex gap-3 h-10 items-center py-7 px-4
+                   hover:bg-zinc-300 cursor-pointer transition-colors'
+    >
       <Image
         src={iconSrc ?? ''}
         alt={title} width={20} height={20}
       />
-      <h2 className='font-semibold'>{title}</h2>
+      <h2 className='font-bold'>{title}</h2>
     </li>
   )
 }
@@ -20,10 +22,12 @@ function Item (props: ItemProps) {
 export function Sidebar () {
   return (
     <aside
-      className='w-50 px-4 pt-3
+      className='w-50 pt-1 border-r border-zinc-300
                 bg-zinc-200'
     >
-      <ul className='flex flex-col gap-1'>
+      <ul className='flex flex-col sidebar
+                    divide-y divide-zinc-300'
+      >
         <Item title='My Page' iconSrc='/images/side_pfp.svg' />
         <Item title='교과과정' iconSrc='/images/side_hat.svg' />
         <Item title='비교과 과정' iconSrc='/images/side_pfp.svg' />
